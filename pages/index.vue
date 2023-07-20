7c39a0613203d8f034c56160487a3a399d3ede95
Alex Bates <ajbates93@gmail.com>
2023-07-20 21:02:14 +0100

    feat: functioning full stack flow

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+const { data: users, pending, refresh } = await useFetch('/api/users')
 
 </script>
 <template>
   <div>
     <div>Hi</div>
+    <div v-if="pending">PENDING</div>
+    <div>Here's some users:</div>
+    <div v-for="user in users">
+      <div>{{ user.name }}</div>
+      <div>{{ user.email }}</div>
+    </div>
   </div>
 </template>
