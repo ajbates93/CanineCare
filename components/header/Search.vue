e6b197023d9fd9ba354f6be4628b5dc495a6c55c
Alex Bates <ajbates93@gmail.com>
2023-08-06 21:03:20 +0100

    feat: nicer keyboard controls for search results

--- components/header/Search.vue
+++ components/header/Search.vue
@@ -5,8 +5,10 @@ const search = ref('')
 const input = ref<HTMLInputElement>()
 const inputContainer = ref<HTMLInputElement>()
 const searchResults = ref<SearchResult[]>([])
+const activeSearchResultIndex = ref<number>(0)
 
 const searchShortcutHandler = (e: KeyboardEvent) => {
+  console.log(e)
   if (e.ctrlKey && e.key === 'k') {
     e.preventDefault()
     input.value?.focus()
@@ -17,6 +19,18 @@ const searchShortcutHandler = (e: KeyboardEvent) => {
     search.value = ''
     input.value?.blur()
     inputContainer.value?.classList.remove('border-custPrimary')
+  }
+  if (e.key === "ArrowDown") {
+    e.preventDefault()
+    activeSearchResultIndex.value++
+    if (activeSearchResultIndex.value > Math.min(5, searchResults.value.length - 1))
+      activeSearchResultIndex.value = 0
+  }
+  if (e.key === "ArrowUp") {
+    e.preventDefault()
+    activeSearchResultIndex.value--
+    if (activeSearchResultIndex.value < 0)
+      activeSearchResultIndex.value = Math.min(5, searchResults.value.length - 1) 
   }
 }
 
@@ -54,10 +68,17 @@ onMounted(() => {
       <span class="bg-gray-200 py-1 px-2 rounded flex items-center text-gray-400 font-bold tracking-wider">CTRL</span>
       <span class="bg-gray-200 py-1 px-2 rounded flex items-center text-gray-400 font-bold tracking-wider">K</span>
     </span>
-    <div class="absolute border border-gray-200 w-full rounded-b left-0 top-full shadow-lg:w
-     bg-gray-100" v-if="searchResults.length > 0">
-      <div class="p-4 flex items-center border-b border-gray-200" v-for="result in searchResults"><Icon name="mdi-arrow-right-bottom" class="text-gray-500 mr-2"></Icon><span class="font-bold mr-1 text-custPrimary">{{ result.name }}</span> - {{ result.age }} Years Old - {{ result.breed }}</div>
-      <div class="px-4 py-2 flex items-center justify-end text-gray-500 text-sm text-right">{{ searchResults.length }} result{{ searchResults.length > 1 ? 's' : '' }}</div>
+    <div v-if="searchResults.length > 0" 
+      class="absolute border border-gray-200 w-full rounded-b left-0 top-full shadow-lg bg-gray-100"
+      >
+      <div class="p-4 flex items-center border-b border-gray-200" v-for="result, idx in searchResults"
+        :class="[{ 'bg-gray-200': idx === activeSearchResultIndex }]">
+        <Icon name="mdi-arrow-right-bottom" class="text-gray-500 mr-2"></Icon>
+        <span class="font-bold mr-1 text-custPrimary">{{ result.name }}</span> - {{ result.age }} Years Old - {{ result.breed }}
+      </div>
+      <div class="px-4 py-2 flex items-center justify-end text-gray-500 text-sm text-right">
+        {{ searchResults.length }} result{{ searchResults.length > 1 ? 's' : '' }}
+      </div>
     </div>
   </div>
 </template>
