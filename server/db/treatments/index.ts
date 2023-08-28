4205a2bb86d0be4dedaa87eee29b8712f8284940
Alex Bates <ajbates93@gmail.com>
2023-08-28 20:36:17 +0100

    feat: update the recurring treatment schema

--- server/db/treatments/index.ts
+++ server/db/treatments/index.ts
@@ -4,5 +4,14 @@ import { CreateRecurringTreatment } from '~/types/treatments'
 const prisma = new PrismaClient()
 
 export const createRecurringTreatmentForDog = async (data: CreateRecurringTreatment) => {
-  const treatment = await prisma.treatment.create(data)
+  const recurringTreatment = {
+    type: data.name,
+    interval: data.interval,
+    intervalValue: data.intervalValue,
+    dogId: data.dogId,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+  await prisma.recurringTreatment.create(recurringTreatment)
+
 }
